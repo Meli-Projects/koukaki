1d4887f66c12b2156584d4afbbe01e9a1bf6a74f
Meli-Projets <melisande77320@gmail.com>
2026-03-24 16:11:02 +0100

    ajustement scss carrousel

--- wp-content/themes/foce-child/assets/js/swiper-init.js
+++ wp-content/themes/foce-child/assets/js/swiper-init.js
@@ -3,13 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
 const swiper = new Swiper('.characters-swiper', {
       effect: "coverflow",
       grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: "auto",
-      loop: true,
+      centeredSlides: false,
+      slidesPerView: "3",
+      spaceBetween: 30, //ajout non fourni avec swiper
+      loop: true, //ajout non fourni avec swiper
       coverflowEffect: {
         rotate: 0,
         stretch: 0,
-        depth: 100,
+        depth: 0,
         modifier: 1,
         slideShadows: false,
       },
